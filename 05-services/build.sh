28aff105310045d9950140aba63014f82b9e58af
nipa <nipa@codefx.org>
2017-04-15 17:04:28 +0200

    [05] Solve

--- 05-services/build.sh
+++ 05-services/build.sh
@@ -9,7 +9,7 @@ rm -rf mods
 mkdir mods
 
 echo " > multi-compiling modules"
-javac9 --module-path libs --module-source-path "./*/src/main/java" -d classes --module monitor
+javac9 --module-path libs --module-source-path "./*/src/main/java" -d classes --module monitor --add-modules monitor.observer.alpha,monitor.observer.beta
 
 echo " > packaging modules"
 jar9 --create --file mods/monitor.observer.jar -C classes/monitor.observer .
