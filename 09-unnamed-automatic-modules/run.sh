b2abbde5f285fe31323f29561f3841fd9490253f
nipa <nipa@codefx.org>
2017-04-19 22:25:53 +0200

    [09] Solve

--- 09-unnamed-automatic-modules/run.sh
+++ 09-unnamed-automatic-modules/run.sh
@@ -7,3 +7,9 @@ echo " > run monitor"
 echo ""
 
 cat libs/tachi
+
+# run in unnamed module
+# java9 --class-path 'mods/*:libs/*' monitor.Main
+
+# run with persistence, rest, and statistics as automatic modules
+java9 --module-path mods --class-path 'libs/*' --module monitor
